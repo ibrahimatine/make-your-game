9967af3685737b6663e4c6334ef3d8c5882fcebd
fmamadou <mmmfall73@gmail.com>
2023-11-20 15:22:41 +0000

    okei

---
+++ app.js
@@ -0,0 +1,12 @@
+// const gameContainer = document.querySelector('#game-container'); 
+
+// function allBricks (nbr){
+//     for (let i = 0; i < nbr; i++) {
+//         const brick = document.createElement('div');
+//         brick.classList.add('brick');
+//         gameContainer.appendChild(brick);
+//     }
+// }
+
+// allBricks(100);
+
