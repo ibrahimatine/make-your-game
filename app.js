c5ed744ceef49a3975f3a3f2aa906afae40405cb
fmamadou <mmmfall73@gmail.com>
2023-11-22 16:39:34 +0000

    add paddle

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-const gameContainer = document.querySelector('#game-container'); 
+conteneur = document.querySelector('#game-container')
+const paddle =  document.createElement('div');
 
-// function allBricks (nbr){
-//     for (let i = 0; i < nbr; i++) {
-//         const brick = document.createElement('div');
-//         brick.classList.add('brick');
-//         gameContainer.appendChild(brick);
-//     }
-// }
+    paddle.style.position= 'absolute';
+    paddle.style.width= '100px';
+    paddle.style.height= '20px';
+    paddle.style.borderRadius= '25px';
+    paddle.style.backgroundColor= 'blue';
+    paddle.style.bottom= '30px';
+    paddle.style.left= '50%';
 
-// allBricks(100);
+    conteneur.appendChild(paddle);
+    
+    document.addEventListener('keydown', function(e) {
+     console.log(e.keyCode);
+    if (e.keyCode===37) paddle.left = true;
+    if (e.keyCode===39) paddle.right = true;
+    })
+    
+    function startGame() {
+        window.requestAnimationFrame(update);
+    }
 
-const movePaddle ();
+    function update() {
+        let pCurrent = paddle.offsetLeft;
+        console.log(pCurrent);
+        if(paddle.left) {
+            pCurrent -= 5;
+        }    
+        paddle.style.left = pCurrent + 'px';
+        window.requestAnimationFrame(update);
+    }    
 
+
+
+
+
+
+
+
+    // function allBricks (nbr){
+    //     for (let i = 0; i < nbr; i++) {
+    //         const brick = document.createElement('div');
+    //         brick.classList.add('brick');
+    //         gameContainer.appendChild(brick);
+    //     }
+    // }
+    
+    // allBricks(100);
