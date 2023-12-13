ea048416108ec30551d7ac008faab668f83d67a8
itine <ibrahimatine29@gmail.com>
2023-12-13 22:47:31 +0000

    new

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ gameover.style.width = "100%";
 gameover.style.backgroundColor = "none";
 gameover.style.textTransform = "uppercase";
 gameover.style.fontSize = "3em";
+gameover.style.display = "block";
+gameover.style.top = "50%";
+gameover.style.transform = "translateY(-50%)";
 
 // Fonction pour démarrer le jeu quand la touche Espace est enfoncée
 function checkStartGame(event) {
@@ -30,8 +33,6 @@ ball.style.height = "20px";
 ball.style.backgroundColor = "black";
 ball.style.borderRadius = "50%";
 ball.style.display = "none";
-ball.style.top = "70%";
-ball.style.left = "50%";
 conteneur.appendChild(ball);
 
 const paddle = document.createElement("div");
@@ -45,24 +46,22 @@ paddle.style.left = "50%";
 paddle.style.transform = "translateX(-50%)";
 conteneur.appendChild(paddle);
 
-
-
 let paddleMovement = {
   left: false,
   right: false,
 };
 
- const keyMap = {
-  ArrowLeft: 'left',
-  ArrowRight: 'right'
+const keyMap = {
+  ArrowLeft: "left",
+  ArrowRight: "right",
 };
 
 // Fonction pour gérer l'événement de pression des touches
 function keyDown(event) {
   const key = keyMap[event.key];
-  if (key === 'left') {
+  if (key === "left") {
     paddleMovement.left = true;
-  } else if (key === 'right') {
+  } else if (key === "right") {
     paddleMovement.right = true;
   }
 }
@@ -70,9 +69,9 @@ function keyDown(event) {
 // Fonction pour gérer l'événement de relâchement des touches
 function keyUp(event) {
   const key = keyMap[event.key];
-  if (key === 'left') {
+  if (key === "left") {
     paddleMovement.left = false;
-  } else if (key === 'right') {
+  } else if (key === "right") {
     paddleMovement.right = false;
   }
 }
@@ -80,7 +79,6 @@ function keyUp(event) {
 // Ajout des écouteurs d'événements optimisés
 document.addEventListener("keydown", keyDown);
 document.addEventListener("keyup", keyUp);
-
 
 const player = {
   gameover: true,
@@ -96,8 +94,8 @@ function startGame() {
     player.score = 0;
     player.lives = 3;
     ball.style.display = "block";
-    player.ballDir = { x: 5, y: 5 };
-    setupBricks(100);
+    player.ballDir = { x: 5, y: -5 }; // Change initial ball direction
+    setupBricks(30); // Adjust number of bricks as per your preference
     scoreUpdater();
     window.requestAnimationFrame(moveBall);
   }
@@ -105,29 +103,24 @@ function startGame() {
 
 function setupBricks(nbr) {
   let rang = {
-    x: (dimensionRec.width % 100) / 2,
+    x: 10,
     y: 50,
   };
-  let saut = false;
+
   for (let x = 0; x < nbr; x++) {
     if (rang.x > dimensionRec.width - 100) {
       rang.y += 50;
-      if (rang.y > dimensionRec.height / 2) {
-        saut = true;
-      }
-      rang.x = (dimensionRec.width % 100) / 2;
+      rang.x = 10;
     }
-    rang.count = x;
-    if (!saut) {
-      createBrick(rang);
-    }
+
+    createBrick(rang);
     rang.x += 100;
   }
 }
 
 function createBrick(pos) {
-  const brick = document.createElement('div');
-  brick.classList.add('brick');
+  const brick = document.createElement("div");
+  brick.classList.add("brick");
 
   brick.style.backgroundColor = "brown";
   brick.style.width = "90px";
@@ -155,21 +148,17 @@ function scoreUpdater() {
   document.querySelector(".score").textContent = player.score;
   document.querySelector(".lives").textContent = player.lives;
 }
-function restartGame() {
-  player.gameover = true;
-  startGame();
-}
 
 function moveBall() {
   const posBall = {
     x: ball.offsetLeft,
     y: ball.offsetTop,
   };
-  
+
   const ballWidth = 20;
   const ballHeight = 20;
-  
-  if (posBall.y > dimensionRec.height - ballHeight || posBall.y < 0) {
+
+  if (posBall.y < 0) {
     player.ballDir.y *= -1;
   }
   if (posBall.x > dimensionRec.width - ballWidth || posBall.x < 0) {
@@ -177,15 +166,17 @@ function moveBall() {
   }
   if (posBall.y > dimensionRec.height - ballHeight) {
     player.lives--;
-    livesDisplay.textContent = `Lives: ${player.lives}`;
+
     if (player.lives <= 0) {
       player.gameover = true;
+      gameover.style.display = "block";
+      return;
     } else {
       // Reset ball position and direction for next life
       ball.style.display = "none";
       posBall.x = dimensionRec.width / 2;
       posBall.y = dimensionRec.height / 2;
-      player.ballDir = { x: 5, y: 5 };
+      player.ballDir = { x: 5, y: -5 }; // Change ball direction for next life
       ball.style.left = posBall.x + "px";
       ball.style.top = posBall.y + "px";
       setTimeout(() => {
@@ -195,21 +186,21 @@ function moveBall() {
       return;
     }
   }
-  
+
   if (collision(paddle, ball)) {
-    const temp = ((posBall.x - paddle.offsetLeft) - (paddle.offsetWidth / 2)) / 10;
+    const temp = (posBall.x - paddle.offsetLeft - paddle.offsetWidth / 2) / 10;
     player.ballDir.x = temp;
     player.ballDir.y *= -1;
   }
 
-  posBall.x += player.ballDir.x * 0.7;
-  posBall.y += player.ballDir.y * 0.7;
+  posBall.x += player.ballDir.x;
+  posBall.y += player.ballDir.y;
 
   ball.style.left = posBall.x + "px";
   ball.style.top = posBall.y + "px";
 
-  const bricks = document.querySelectorAll('.brick');
-  bricks.forEach(brick => {
+  const bricks = document.querySelectorAll(".brick");
+  bricks.forEach((brick) => {
     if (collision(brick, ball)) {
       player.ballDir.y *= -1;
       brick.parentNode.removeChild(brick);
@@ -218,24 +209,87 @@ function moveBall() {
     }
   });
 
-
   let position = paddle.offsetLeft;
   const paddleWidth = paddle.offsetWidth;
-  const paddleMoveSpeed = 5;
+  const paddleMoveSpeed = 10;
 
   if (paddleMovement.left && position > 0) {
     position = Math.max(0, position - paddleMoveSpeed);
   }
   if (paddleMovement.right && position < dimensionRec.width - paddleWidth) {
-    position = Math.min(dimensionRec.width - paddleWidth, position + paddleMoveSpeed);
+    position = Math.min(
+      dimensionRec.width - paddleWidth,
+      position + paddleMoveSpeed
+    );
   }
 
   paddle.style.left = position + "px";
-
 
   if (!player.gameover) {
     window.requestAnimationFrame(moveBall);
   } else {
     gameover.style.display = "block";
   }
+}
+// ... (Le code précédent reste le même)
+
+// ... (Le code précédent reste le même)
+
+document.body.appendChild(conteneur);
+
+// Création des éléments pour afficher le score et les vies
+const scoreDisplay = document.createElement("div");
+scoreDisplay.textContent = "Score: ";
+scoreDisplay.classList.add("score");
+scoreDisplay.style.position = "absolute";
+scoreDisplay.style.top = "10px";
+scoreDisplay.style.left = "10px";
+scoreDisplay.style.color = "black";
+conteneur.appendChild(scoreDisplay);
+
+const livesDisplay = document.createElement("div");
+livesDisplay.textContent = "Lives: ";
+livesDisplay.classList.add("lives");
+livesDisplay.style.position = "absolute";
+livesDisplay.style.top = "10px";
+livesDisplay.style.right = "10px";
+livesDisplay.style.color = "black";
+conteneur.appendChild(livesDisplay);
+
+// Appeler la fonction pour démarrer le jeu au chargement de la page
+startGame();
+
+// Écouter l'événement de redimensionnement de la fenêtre pour mettre à jour les dimensions du conteneur
+window.addEventListener("resize", () => {
+  dimensionRec = conteneur.getBoundingClientRect();
+});
+
+// Réinitialiser le jeu lorsque l'utilisateur clique sur l'écran (utile si le jeu est sur mobile)
+conteneur.addEventListener("click", () => {
+  if (player.gameover) {
+    restartGame();
+  }
+});
+
+// Fonction pour détecter la fin du jeu et afficher un message approprié
+function endGame() {
+  const endMessage = document.createElement("div");
+  endMessage.textContent = "Game Over!";
+  endMessage.style.position = "absolute";
+  endMessage.style.top = "50%";
+  endMessage.style.left = "50%";
+  endMessage.style.transform = "translate(-50%, -50%)";
+  endMessage.style.fontSize = "3em";
+  endMessage.style.color = "red";
+  conteneur.appendChild(endMessage);
+}
+
+// Mettre à jour le décompte des vies
+function updateLivesDisplay() {
+  livesDisplay.textContent = `Lives: ${player.lives}`;
+}
+
+// Mettre à jour le décompte du score
+function updateScoreDisplay() {
+  scoreDisplay.textContent = `Score: ${player.score}`;
 }
